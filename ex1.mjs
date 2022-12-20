f9952af8614e88c120664018c1fac31bec9696d2
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-21 08:45:09 +0900

    외부 모듈 써보기

---
+++ ex1.mjs
@@ -0,0 +1,6 @@
+import * as loggerModule from './logger.mjs';
+console.log(loggerModule);
+console.log(loggerModule.DEFAULT_LEVEL);
+console.log(loggerModule.LEVELS);
+console.log(loggerModule.Logger);
+console.log(loggerModule.log);
